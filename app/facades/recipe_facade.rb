ba87ca60133a22703b06454da69f9f2afe174b3c
Billy Wallace <wallace.billy1@gmail.com>
2024-06-09 11:19:18 -0600

    Initial setup and implementation of recipe index controller action

--- app/facades/recipe_facade.rb
+++ app/facades/recipe_facade.rb
@@ -1,14 +1,3 @@
-# class RecipeFacade
-#   def recipes_by_country(country)
-#     json = RecipeService.new
-#     require 'pry'; binding.pry
-    
-#     json.search(country)
-
-#     @recipes = json[:hits].map { |details| Recipe.new(details, country) }
-#   end
-# end
-
 class RecipeFacade
   def recipes_by_country(country)
     json = service.search(country)
--- spec/requests/api/v1/ingredients_request_spec.rb
+++ spec/requests/api/v1/ingredients_request_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Recipes API" do
-  before(:each) do
-    xit "sends a list of recipes from a country" do
-      get '/api/v1/recipes?country=thailand'
+  describe "Recipe Index" do
+    it "returns all relevant recipe attributes for recipes from a state search", :vcr do
+      get "/api/v1/recipes?country=thailand"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(data.first[:id]).to be(nil)
+      expect(data.first[:type]).to eq("recipe")
+      expect(data.first[:attributes][:title]).to be_a(String)
+      expect(data.first[:attributes][:url]).to be_a(String)
+      expect(data.first[:attributes][:country]).to eq("thailand")
+      expect(data.first[:attributes][:image]).to be_a(String)
     end
   end
 end
