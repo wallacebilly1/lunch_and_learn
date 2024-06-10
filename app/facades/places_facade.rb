aa4f2fab915da3ba21179266b6e62a32e86dfe26
Billy Wallace <wallace.billy1@gmail.com>
2024-06-10 09:31:33 -0600

    Create Places Facade

---
+++ app/facades/places_facade.rb
@@ -0,0 +1,12 @@
+class PlacesFacade
+  def tourist_sites_from(lat, lon)
+    json = service.tourist_sites_from(lat, lon)
+    @places = json[:features].map { |details| Place.new(details) }
+  end
+
+  private
+
+  def service
+    PlaceService.new
+  end
+end
