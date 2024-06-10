16daed63cfcdb59cc6b30817518ae69ba0a7611d
Billy Wallace <wallace.billy1@gmail.com>
2024-06-10 13:37:27 -0600

    Add association test to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :password }
     it { should validate_presence_of :password_confirmation}
     it { should have_secure_password }
+    it { should have_many :favorites }
     it { should allow_value('something@something.something').for(:email) }
     it { should_not allow_value('something somthing@something.something').for(:email) }
     it { should_not allow_value('something.something@').for(:email) }
